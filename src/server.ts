4aaf55fd1b221f2b923a060cd46336a2d4f1dc35
Nikhil Tanwar <nikhiltanwarnt@gmail.com>
2025-04-04 18:35:33 -0400

    add Express server setup

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,15 @@
-// function welcome(name: string) {
-//   console.log("Welcome", name);
-// }
-
-// welcome("Nikhil T");
-
 import { Config } from "./config";
-console.log(Config.PORT);
+import app from "./app";
+const startServer = () => {
+  const PORT = Config.PORT;
+  try {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (err) {
+    console.error("Error starting server:", err);
+    process.exit(1);
+  }
+};
+
+startServer();
