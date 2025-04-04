ee1e47ed37ef37050d8e451c903bbd9cfbb75a62
Nikhil Tanwar <nikhiltanwarnt@gmail.com>
2025-04-04 18:22:23 -0400

    add dotenv configuration and example environment file

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
-function welcome(name: string) {
-  console.log("Welcome", name);
-}
+// function welcome(name: string) {
+//   console.log("Welcome", name);
+// }
 
-welcome("Nikhil T");
+// welcome("Nikhil T");
+
+import { Config } from "./config";
+console.log(Config.PORT);
