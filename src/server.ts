c7eb4dea2ff8f0cf586fc2e4276e7f18d6571a4d
Nikhil Tanwar <nikhiltanwarnt@gmail.com>
2025-04-04 23:00:02 -0400

    add Winston logger for improved logging in the application

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import { Config } from "./config";
 import app from "./app";
+import logger from "./config/logger";
 const startServer = () => {
   const PORT = Config.PORT;
   try {
     app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
+      logger.info("Server listening on port", { port: PORT });
     });
   } catch (err) {
     console.error("Error starting server:", err);
