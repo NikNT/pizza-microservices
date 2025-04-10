1ffc983162abda303fd1c933394fec08e0c717a1
Nikhil Tanwar <nikhiltanwarnt@gmail.com>
2025-04-10 19:47:22 -0400

    implement user registration with validation and service layer; add JSON parsing middleware

--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -1,7 +1,25 @@
 import request from "supertest";
 import app from "../../src/app";
+import { DataSource } from "typeorm";
+import { AppDataSource } from "../../src/config/data-source";
+import { truncateTables } from "../utils/index";
+import { User } from "../../src/entity/User";
 
 describe("POST /auth/register", () => {
+  let connection: DataSource;
+
+  beforeAll(async () => {
+    connection = await AppDataSource.initialize();
+  });
+
+  beforeEach(async () => {
+    //Database truncation
+    await truncateTables(connection);
+  });
+
+  afterAll(async () => {
+    await connection.destroy();
+  });
   describe("Given all fields", () => {
     it("should return 201", async () => {
       // AAA - Arrange, Act, Assert
@@ -52,6 +70,13 @@ describe("POST /auth/register", () => {
       await request(app).post("/auth/register").send(userData);
 
       // Assert
+      const userRepository = connection.getRepository(User);
+      const users = await userRepository.find();
+
+      expect(users.length).toBe(1);
+      expect(users[0].firstName).toBe(userData.firstName);
+      expect(users[0].lastName).toBe(userData.lastName);
+      expect(users[0].email).toBe(userData.email);
     });
   });
 
