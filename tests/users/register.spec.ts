3395691a02b50ab81cf42a1977f8ceb41b4f517e
Nikhil Tanwar <nikhiltanwarnt@gmail.com>
2025-04-12 19:41:49 -0400

    add email sanitization and add test for trimming functionality

--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -182,4 +182,26 @@ describe("POST /auth/register", () => {
       expect(users).toHaveLength(0);
     });
   });
+
+  describe("Fields are in invalid format", () => {
+    it("should trim the email field", async () => {
+      // Arrange
+      const userData = {
+        firstName: "John",
+        lastName: "Doe",
+        email: " dvHb0g@example.com ",
+        password: "password123",
+      };
+
+      //Act
+      await request(app).post("/auth/register").send(userData);
+
+      //Assert
+      const userRepository = connection.getRepository(User);
+      const users = await userRepository.find();
+      const user = users[0];
+
+      expect(user.email).toBe("dvHb0g@example.com");
+    });
+  });
 });
