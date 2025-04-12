e42970d6057e854e8e618c9efeaffcb89d290b69
Nikhil Tanwar <nikhiltanwarnt@gmail.com>
2025-04-11 22:44:05 -0400

    add express-validator for user registration validation and enhance error handling

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,9 +1,10 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { AuthController } from "../controllers/AuthController";
 import { UserService } from "../services/UserService";
 import { AppDataSource } from "../config/data-source";
 import { User } from "../entity/User";
 import logger from "../config/logger";
+import registerValidator from "../validators/register-validator";
 
 const router = express.Router();
 
@@ -13,7 +14,15 @@ const userService = new UserService(userRepository);
 const authController = new AuthController(userService, logger);
 
 // Routes
-router.post("/register", (req, res, next) =>
-  authController.register(req, res, next),
+router.post(
+  "/register",
+  registerValidator,
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await authController.register(req, res, next);
+    } catch (error) {
+      next(error);
+    }
+  },
 );
 export default router;
--- tests/users/register.spec.ts
+++ tests/users/register.spec.ts
@@ -162,5 +162,24 @@ describe("POST /auth/register", () => {
     });
   });
 
-  describe("Fields are missing", () => {});
+  describe("Fields are missing", () => {
+    it("should return 400 status code if email field is missing", async () => {
+      // Arrange
+      const userData = {
+        firstName: "John",
+        lastName: "Doe",
+        email: "",
+        password: "password123",
+      };
+
+      //Act
+      const response = await request(app).post("/auth/register").send(userData);
+
+      //Assert
+      expect(response.status).toBe(400);
+      const userRepository = connection.getRepository(User);
+      const users = await userRepository.find();
+      expect(users).toHaveLength(0);
+    });
+  });
 });
