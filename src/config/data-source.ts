5aeb787d7095c120f0f6f46c200324ebdff58cac
Nikhil Tanwar <nikhiltanwarnt@gmail.com>
2025-04-27 18:25:35 -0400

    feat(migration): add user and refresh_token tables with migration script

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -12,9 +12,9 @@ export const AppDataSource = new DataSource({
   password: Config.DB_PASSWORD,
   database: Config.DB_NAME,
   // IMPORTANT : Don't use this in production. Always keep FALSE in production.
-  synchronize: true,
+  synchronize: false,
   logging: false,
   entities: [User, RefreshToken],
-  migrations: [],
+  migrations: ["src/migration/*.ts"],
   subscribers: [],
 });
